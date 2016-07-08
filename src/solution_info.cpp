481e8fcea0a44a1272a31970182b449b2553b4ad
Brian Granzow <brian.granzow@gmail.com>
2016-07-08 19:23:20 -0400

    solution info : implement methods

---
+++ src/solution_info.cpp
@@ -0,0 +1,57 @@
+#include "solution_info.hpp"
+#include "mesh.hpp"
+#include "control.hpp"
+
+namespace goal {
+
+void SolutionInfo::resize(
+    RCP<Mesh> mesh,
+    bool enable_dynamics)
+{
+  double t0 = time();
+  int num_vectors = 1;
+  if (enable_dynamics) num_vectors = 3;
+  RCP<const Map> m = mesh->get_owned_map();
+  RCP<const Map> om = mesh->get_overlap_map();
+  RCP<const Graph> g = mesh->get_owned_graph();
+  RCP<const Graph> og = mesh->get_overlap_graph();
+  exporter = rcp(new Export(om,m));
+  importer = rcp(new Import(m,om));
+  owned_solution = rcp(new MultiVector(m, num_vectors));
+  owned_residual = rcp(new Vector(m));
+  owned_jacobian = rcp(new Matrix(g));
+  ovlp_solution = rcp(new MultiVector(om, num_vectors));
+  ovlp_residual = rcp(new Vector(om));
+  ovlp_jacobian = rcp(new Matrix(og));
+  double t1 = time();
+  print("solution containers resized in %f seconds", t1-t0);
+}
+
+void SolutionInfo::gather_solution()
+{
+  owned_solution->doExport(*ovlp_solution, *exporter, Tpetra::INSERT);
+}
+
+void SolutionInfo::scatter_solution()
+{
+  ovlp_solution->doImport(*owned_solution, *importer, Tpetra::INSERT);
+}
+
+void SolutionInfo::gather_residual()
+{
+  owned_residual->doExport(*ovlp_residual, *exporter, Tpetra::ADD);
+}
+
+void SolutionInfo::gather_jacobian()
+{
+  owned_jacobian->doExport(*ovlp_jacobian, *exporter, Tpetra::ADD);
+}
+
+RCP<SolutionInfo> sol_info_create(RCP<Mesh> m, bool enable_dynamics)
+{
+  RCP<SolutionInfo> s = rcp(new SolutionInfo);
+  s->resize(m, enable_dynamics);
+  return s;
+}
+
+}
