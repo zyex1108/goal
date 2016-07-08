f62d2801190d21afac8fdddd4a7aacd7606935ac
Brian Granzow <brian.granzow@gmail.com>
2016-07-08 19:22:29 -0400

    solution info : add helper methods

--- src/solution_info.hpp
+++ src/solution_info.hpp
@@ -2,19 +2,34 @@
 #define goal_solution_info_hpp
 
 #include "data_types.hpp"
+#include "Teuchos_RCP.hpp"
 
 namespace goal {
+
+using Teuchos::rcp;
+using Teuchos::RCP;
+
+class Mesh;
 
 class SolutionInfo
 {
   public:
-    RCP<MulitiVector> owned_solution;
+    void resize(RCP<Mesh> m, bool enable_dynamics);
+    void gather_solution();
+    void scatter_solution();
+    void gather_residual();
+    void gather_jacobian();
+    RCP<MultiVector> owned_solution;
     RCP<Vector> owned_residual;
     RCP<Matrix> owned_jacobian;
     RCP<MultiVector> ovlp_solution;
     RCP<Vector> ovlp_residual;
-    RCP<Vector> ovlp_jacobain;
+    RCP<Matrix> ovlp_jacobian;
+    RCP<Export> exporter;
+    RCP<Import> importer;
 };
+
+RCP<SolutionInfo> sol_info_create(RCP<Mesh> m, bool enable_dynamics);
 
 }
 
