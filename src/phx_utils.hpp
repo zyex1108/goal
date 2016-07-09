087450bc91c61dff8ae6aed381ea8d76d3470c19
Brian Granzow <brian.granzow@gmail.com>
2016-07-09 11:23:18 -0400

    begin phx utils

---
+++ src/phx_utils.hpp
@@ -0,0 +1,23 @@
+#ifndef goal_phx_utils_hpp
+#define goal_phx_utils_hpp
+
+#include "dimension.hpp"
+#include "layouts.hpp"
+#include <Phalanx.hpp>
+
+namespace goal {
+
+template <typename ScalarT>
+void get_grad_field(
+    PHX::MDField<ScalarT, Elem, Node, QP> f,
+    RCP<Layouts> dl,
+    PHX::MDField<ScalarT, Elem, Node, QP, Dim>& gf)
+{
+  std::string const& name = f.fieldTag().name();
+  gf = PHX::MDField<ScalarT, Elem, Node, QP, Dim>
+    ("grad_" + name, dl->node_qp_vector);
+}
+
+}
+
+#endif
