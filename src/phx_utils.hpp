9b8c8ada2dc93cb948ef1c916166ccb4ea87837e
Brian Granzow <brian.granzow@gmail.com>
2016-07-11 08:33:13 -0400

    add more get_field* methods to phx_utils

--- src/phx_utils.hpp
+++ src/phx_utils.hpp
@@ -17,12 +17,41 @@ void get_field(
 }
 
 template <typename ScalarT>
+void get_field(
+    std::string const& name,
+    RCP<Layouts> dl,
+    PHX::MDField<ScalarT,Elem,QP>& f)
+{
+  f = PHX::MDField<ScalarT,Elem,QP>(name,dl->qp_scalar);
+}
+
+template <typename ScalarT>
 void get_grad_field(
     PHX::MDField<ScalarT, Elem, Node, QP> f,
     RCP<Layouts> dl,
     PHX::MDField<ScalarT, Elem, Node, QP, Dim>& gf)
 {
   std::string const& name = f.fieldTag().name();
+  gf = PHX::MDField<ScalarT, Elem, Node, QP, Dim>
+    ("grad_" + name, dl->node_qp_vector);
+}
+
+template <typename ScalarT>
+void get_grad_field(
+    std::string const& name,
+    RCP<Layouts> dl,
+    PHX::MDField<ScalarT, Elem, QP, Dim>& gf)
+{
+  gf = PHX::MDField<ScalarT, Elem, QP, Dim>
+    ("grad_" + name, dl->qp_vector);
+}
+
+template <typename ScalarT>
+void get_grad_field(
+    std::string const& name,
+    RCP<Layouts> dl,
+    PHX::MDField<ScalarT, Elem, Node, QP, Dim>& gf)
+{
   gf = PHX::MDField<ScalarT, Elem, Node, QP, Dim>
     ("grad_" + name, dl->node_qp_vector);
 }
