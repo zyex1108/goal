6a48f0a5b7a87f9a201e8841b1396877fa136894
Brian Granzow <brian.granzow@gmail.com>
2016-07-11 21:48:58 -0400

    add scatter residuals

---
+++ src/ev_scatter_residual.cpp
@@ -0,0 +1,139 @@
+#include "ev_scatter_residual.hpp"
+#include "mesh.hpp"
+#include "control.hpp"
+#include "workset.hpp"
+#include "layouts.hpp"
+#include "phx_utils.hpp"
+
+namespace goal {
+
+using Teuchos::ArrayRCP;
+using Teuchos::arrayView;
+
+template <typename Traits>
+ScatterResidual<GoalTraits::Residual, Traits>::
+ScatterResidual(ParameterList const& p) :
+  dl        (p.get<RCP<Layouts> >("Layouts")),
+  mesh      (p.get<RCP<Mesh> >("Mesh")),
+  dof_names (p.get<Teuchos::Array<std::string> >("DOF Names"))
+{
+  num_nodes = dl->node_vector->dimension(1);
+  num_eqs = dof_names.size();
+
+  resid.resize(num_eqs);
+  for (unsigned i=0; i < num_eqs; ++i) {
+    get_resid_field(dof_names[i], dl, resid[i]);
+    this->addDependentField(resid[i]);
+  }
+
+  std::string name = "Scatter Residual";
+  PHX::Tag<ScalarT> op(name, dl->dummy);
+  this->addEvaluatedField(op);
+  this->setName(name);
+}
+
+template <typename Traits>
+void ScatterResidual<GoalTraits::Residual, Traits>::
+postRegistrationSetup(
+    typename Traits::SetupData d,
+    PHX::FieldManager<Traits>& fm)
+{
+  for (unsigned i=0; i < num_eqs; ++i)
+    this->utils.setFieldData(resid[i], fm);
+}
+
+template <typename Traits>
+void ScatterResidual<GoalTraits::Residual, Traits>::
+evaluateFields(typename Traits::EvalData workset)
+{
+  CHECK(workset.r != Teuchos::null);
+  ArrayRCP<ST> r = workset.r->get1dViewNonConst();
+  CHECK(r != Teuchos::null);
+
+  for (unsigned elem=0; elem < workset.size; ++elem) {
+    apf::MeshEntity* e= workset.ents[elem];
+    for (unsigned node=0; node < num_nodes; ++node) {
+      for (unsigned eq=0; eq < num_eqs; ++eq) {
+        LO lid = mesh->get_lid(e, node, eq);
+        r[lid] += resid[eq](elem, node);
+      }
+    }
+  }
+}
+
+template <typename Traits>
+ScatterResidual<GoalTraits::Jacobian, Traits>::
+ScatterResidual(ParameterList const& p) :
+  dl        (p.get<RCP<Layouts> >("Layouts")),
+  mesh      (p.get<RCP<Mesh> >("Mesh")),
+  dof_names (p.get<Teuchos::Array<std::string> >("DOF Names"))
+{
+  num_nodes = dl->node_vector->dimension(1);
+  num_eqs = dof_names.size();
+  num_dofs = num_nodes * num_eqs;
+
+  resid.resize(num_eqs);
+  for (unsigned i=0; i < num_eqs; ++i) {
+    get_resid_field(dof_names[i], dl, resid[i]);
+    this->addDependentField(resid[i]);
+  }
+
+  std::string name = "Scatter Residual";
+  PHX::Tag<ScalarT> op(name, dl->dummy);
+  this->addEvaluatedField(op);
+  this->setName(name);
+}
+
+template <typename Traits>
+void ScatterResidual<GoalTraits::Jacobian, Traits>::
+postRegistrationSetup(
+    typename Traits::SetupData d,
+    PHX::FieldManager<Traits>& fm)
+{
+  for (unsigned i=0; i < num_eqs; ++i)
+    this->utils.setFieldData(resid[i], fm);
+}
+
+template <typename Traits>
+void ScatterResidual<GoalTraits::Jacobian, Traits>::
+evaluateFields(typename Traits::EvalData workset)
+{
+  CHECK(workset.J != Teuchos::null);
+  RCP<Matrix> J = workset.J;
+
+  bool fill_resid = false;
+  ArrayRCP<ST> r;
+  if (workset.r != Teuchos::null) {
+    fill_resid = true;
+    r = workset.r->get1dViewNonConst();
+    CHECK(r != Teuchos::null);
+  }
+
+  Teuchos::Array<LO> cols(num_dofs);
+  for (unsigned elem=0; elem < workset.size; ++elem) {
+    apf::MeshEntity* e = workset.ents[elem];
+    
+    unsigned idx=0;
+    for (unsigned node=0; node < num_nodes; ++node) {
+      for (unsigned eq=0; eq < num_eqs; ++eq) {
+        cols[idx] = mesh->get_lid(e, node, eq);
+        idx++;
+      }
+    }
+
+    for (unsigned node=0; node < num_nodes; ++node) {
+      for (unsigned eq=0; eq < num_eqs; ++eq) {
+        LO row = mesh->get_lid(e, node, eq);
+        FadType v = resid[eq](elem, node);
+        J->sumIntoLocalValues(
+            row, cols, arrayView(&(v.fastAccessDx(0)), num_dofs));
+        if (fill_resid)
+          r[row] += resid[eq](elem, node).val();
+      }
+    }
+  }
+}
+
+GOAL_INSTANTIATE_ALL(ScatterResidual)
+
+}
