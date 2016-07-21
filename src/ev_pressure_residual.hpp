ab13252aff2a047740717af2f6e77ce6c61da845
Brian Granzow <brian.granzow@gmail.com>
2016-07-20 20:46:32 -0400

    first attempt at filling in pressure residual

--- src/ev_pressure_residual.hpp
+++ src/ev_pressure_residual.hpp
@@ -9,13 +9,21 @@ using Teuchos::RCP;
 using Teuchos::ParameterList;
 
 struct Layouts;
+class Mesh;
 
 PHX_EVALUATOR_CLASS(PressureResidual)
 
   private:
 
     RCP<Layouts> dl;
+    RCP<Mesh> mesh;
+    RCP<const ParameterList> params;
+    bool small_strain;
     std::string pressure_name;
+
+    double G;
+    double K;
+    double alpha;
 
     unsigned num_nodes;
     unsigned num_qps;
@@ -25,6 +33,7 @@ PHX_EVALUATOR_CLASS(PressureResidual)
     PHX::MDField<double, Elem, Node, QP> BF;
     PHX::MDField<double, Elem, Node, QP, Dim> gBF;
     PHX::MDField<ScalarT, Elem, QP, Dim, Dim> def_grad;
+    PHX::MDField<ScalarT, Elem, QP> det_def_grad;
     PHX::MDField<ScalarT, Elem, QP, Dim, Dim> stress;
     PHX::MDField<ScalarT, Elem, QP> pressure;
     PHX::MDField<ScalarT, Elem, QP, Dim> pressure_grad;
