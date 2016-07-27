dbc1db4ddf66f2c6ce7dc95b4cd66d3277bf72a2
Brian Granzow <brian.granzow@gmail.com>
2016-07-27 08:28:32 -0400

    some progress on creep model

--- src/ev_model_creep.cpp
+++ src/ev_model_creep.cpp
@@ -16,7 +16,6 @@ static RCP<ParameterList> get_valid_params()
   p->set<double>("K", 0.0);
   p->set<double>("Y", 0.0);
   p->set<double>("A", 0.0);
-  p->set<double>("B", 0.0);
   p->set<double>("QR", 0.0);
   p->set<double>("C2", 0.0);
   return p;
@@ -29,7 +28,6 @@ static void validate_params(RCP<const ParameterList> p)
   assert_param(p, "K");
   assert_param(p, "Y");
   assert_param(p, "A");
-  assert_param(p, "B");
   assert_param(p, "QR");
   assert_param(p, "C2");
   p->validateParameters(*get_valid_params(), 0);
@@ -49,7 +47,6 @@ PHX_EVALUATOR_CTOR(ModelCreep, p) :
   K = params->get<double>("K");
   Y = params->get<double>("Y");
   A = params->get<double>("A");
-  B = params->get<double>("B");
   QR = params->get<double>("QR");
   C2 = params->get<double>("C2");
 
@@ -72,6 +69,76 @@ PHX_POST_REGISTRATION_SETUP(ModelCreep, data, fm)
 
 PHX_EVALUATE_FIELDS(ModelCreep, workset)
 {
+  /* parameters */
+  ScalarT kappa = E/(3.0*(1.0-2.0*nu));
+  ScalarT mu = E/(2.0*(1.0+nu));
+  ScalarT sq23(std::sqrt(2.0/3.0));
+
+  /* quantities at previous time */
+  ScalarT eqps;
+  Intrepid2::Tensor<ScalarT> Fp(num_dims);
+  Intrepid2::Tensor<ScalarT> Fpinv(num_dims);
+  Intrepid2::Tensor<ScalarT> Cpinv(num_dims);
+
+  /* quantities at current time */
+  ScalarT J;
+  ScalarT Jm23;
+  ScalarT dgam;
+  ScalarT dgamp;
+  Intrepid2::Tensor<ScalarT> F(num_dims);
+  Intrepid2::Tensor<ScalarT> Fpn(num_dims);
+  Intrepid2::Tensor<ScalarT> N(num_dims);
+  Intrepid2::Tensor<ScalarT> sigma(num_dims);
+  Intrepid2::Tensor<ScalarT> A(num_dims);
+  Intrepid2::Tensor<ScalarT> expA(num_dims);
+  Intrepid2::Tensor<ScalarT> I(Intrepid2::eye<ScalarT>(num_dims));
+
+  /* trial state quantities */
+  ScalarT f;
+  ScalarT a0;
+  ScalarT a1;
+  ScalarT mubar;
+  Intrepid2::Tensor<ScalarT> be(num_dims);
+  Intrepid2::Tensor<ScalarT> s(num_dims);
+
+  for (unsigned elem=0; elem < workset.size; ++elem) {
+
+    apf::MeshEntity* e = workset.ents[elem];
+
+    for (unsigned qp=0; qp < num_qps; ++qp) {
+
+      /* compute the temperature adjusted relaxation parameter */
+      B = A*std::exp(-QR / 303.0);
+
+      /* deformation gradient quantities */
+      for (unsigned i=0; i < num_dims; ++i)
+      for (unsigned j=0; j < num_dims; ++j)
+        F(i,j) = def_grad(elem, qp, i ,j);
+      J = det_def_grad(elem, qp);
+      Jm23 = std::pow(J, -2.0/3.0);
+
+      /* get the plastic part of the def grad quantities */
+      states->get_tensor("Fp_old", e, qp, Fp);
+      Fpinv = Intrepid2::inverse(Fp);
+
+      /* compute the trial state */
+      Cpinv = Fpinv*Intrepid2::transpose(Fpinv);
+      be = Jm23*F*Cpinv*Intrepid2::transpose(F);
+      s = mu*Intrepid2::dev(be);
+      mubar = Intrepid2::trace(be)*mu/num_dims;
+
+      /* compute plasticity yield criteria */
+      ScalarT smag = Intrepid2::norm<ScalarT>(s);
+      states->get_scalar("eqps_old", e, qp, eqps);
+      f = smag - sq23*(Y + K*eqps);
+
+      /* compute creep onset criteria */
+      a0 = Intrepid2::norm(Intrepid2::dev(be));
+      a1 = Intrepid2::trace(be);
+    }
+  }
+
+
 }
 
 }
