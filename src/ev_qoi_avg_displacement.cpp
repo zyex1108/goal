a971f42563b27d472322311a8fea831be0e54eaa
Brian Granzow <brian.granzow@gmail.com>
2016-07-31 10:14:35 -0400

    fill in avg displacment qoi methods

--- src/ev_qoi_avg_displacement.cpp
+++ src/ev_qoi_avg_displacement.cpp
@@ -12,14 +12,38 @@ PHX_EVALUATOR_CTOR(QoIAvgDisplacement, p) :
   wDv         (p.get<std::string>("Weighted Dv Name"), dl->qp_scalar),
   avg_disp    (p.get<std::string>("Avg Displacement Name"), dl->elem_scalar)
 {
+  num_nodes = dl->node_qp_vector->dimension(1);
+  num_qps = dl->node_qp_vector->dimension(2);
+  num_dims = dl->node_qp_vector->dimension(3);
+
+  disp.resize(disp_names.size());
+  for (unsigned i=0; i < disp_names.size(); ++i) {
+    get_field(disp_names[i], dl, disp[i]);
+    this->addDependentField(disp[i]);
+  }
+
+  this->addDependentField(wDv);
+  this->addEvaluatedField(avg_disp);
+  this->setName("QoI: Average Displacement");
 }
 
 PHX_POST_REGISTRATION_SETUP(QoIAvgDisplacement, data, fm)
 {
+  this->utils.setFieldData(wDv, fm);
+  for (unsigned i=0; i < disp_names.size(); ++i)
+    this->utils.setFieldData(disp[i], fm);
+  this->utils.setFieldData(avg_disp, fm);
 }
 
 PHX_EVALUATE_FIELDS(QoIAvgDisplacement, workset)
 {
+  for (unsigned elem=0; elem < workset.size; ++elem) {
+    avg_disp(elem) = 0.0;
+    for (unsigned qp=0; qp < num_qps; ++qp)
+    for (unsigned i=0; i < num_dims; ++i)
+      avg_disp(elem) += disp[i](elem, qp) * wDv(elem, qp);
+    avg_disp(elem) /= num_dims;
+  }
 }
 
 GOAL_INSTANTIATE_JACOBIAN(QoIAvgDisplacement)
