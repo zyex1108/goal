2bb80c7309a1ade2c5f7839b1c2680e89b055441
Brian Granzow <brian.granzow@gmail.com>
2016-07-31 17:44:16 -0400

    fill in scatter qoi evaluator

--- src/ev_scatter_qoi.cpp
+++ src/ev_scatter_qoi.cpp
@@ -6,15 +6,18 @@
 
 namespace goal {
 
+using Teuchos::ArrayRCP;
+
 template <typename Traits>
 ScatterQoI<GoalTraits::Forward, Traits>::
 ScatterQoI(ParameterList const& p) :
   dl      (p.get<RCP<Layouts> >("Layouts")),
   qoi     (p.get<std::string>("QoI Name"), dl->elem_scalar)
 {
-  this->addDependentField(qoi);
   std::string name = "Scatter QoI";
   PHX::Tag<ScalarT> op(name, dl->dummy);
+  this->addDependentField(qoi);
+  this->addEvaluatedField(op);
   this->setName("name");
 }
 
@@ -40,9 +43,12 @@ ScatterQoI(ParameterList const& p) :
   mesh    (p.get<RCP<Mesh> >("Mesh")),
   qoi     (p.get<std::string>("QoI Name"), dl->elem_scalar)
 {
-  this->addDependentField(qoi);
+
+
   std::string name = "Scatter QoI";
   PHX::Tag<ScalarT> op(name, dl->dummy);
+  this->addDependentField(qoi);
+  this->addEvaluatedField(op);
   this->setName("name");
 }
 
@@ -59,6 +65,22 @@ template <typename Traits>
 void ScatterQoI<GoalTraits::Derivative, Traits>::
 evaluateFields(typename Traits::EvalData workset)
 {
+  CHECK(workset.q != Teuchos::null);
+  ArrayRCP<ST> dqdu = workset.q->get1dViewNonConst();
+  CHECK(dqdu != Teuchos::null);
+
+  unsigned num_eqs = mesh->get_num_eqs();
+  for (unsigned elem=0; elem < workset.size; ++elem) {
+    apf::MeshEntity* e = workset.ents[elem];
+    unsigned dof = 0;
+    for (unsigned node=0; node < num_nodes; ++node) {
+      for (unsigned eq=0; eq < num_eqs; ++eq) {
+        LO row = mesh->get_lid(e, node, eq);
+        dqdu[row] += qoi(elem).fastAccessDx(dof);
+        ++dof;
+      }
+    }
+  }
 }
 
 GOAL_INSTANTIATE_ALL(ScatterQoI)
