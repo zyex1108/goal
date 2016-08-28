7bd4143247fb41ada6c7d25d55384120655fba92
Brian Granzow <brian.granzow@gmail.com>
2016-08-28 16:55:53 -0400

    begin mechanics error evaluator

---
+++ src/ev_mechanics_error.hpp
@@ -0,0 +1,33 @@
+#ifndef goal_mechanics_error_hpp
+#define goal_mechanics_error_hpp
+
+#include "phx_macros.hpp"
+#include "traits.hpp"
+
+namespace goal {
+
+using Teuchos::RCP;
+using Teuchos::ArrayRCP;
+using Teuchos::ParameterList;
+
+class Mesh;
+class Layouts;
+
+PHX_EVALUATOR_CLASS(MechanicsError)
+
+  private:
+
+    Teuchos::RCP<Layouts> dl;
+    Teuchos::RCP<Mesh> mesh;
+
+    unsigned num_nodes;
+    unsigned num_qps;
+    unsigned num_dims;
+
+    PHX::MDField<ScalarT, Elem, QP, Dim, Dim> first_pk;
+
+PHX_EVALUATOR_CLASS_END
+
+}
+
+#endif
